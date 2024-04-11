388499202fa6ee6cef16d74d027a8e854e700576
tushar-upadhya <tusharupadhyay691@gmail.com>
2024-04-11 17:35:18 +0530

    db

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
 
 export const registerAuth = async (req, res) => {
   const { username, email, password } = req.body;
@@ -7,8 +8,16 @@ export const registerAuth = async (req, res) => {
   const hasPassword = await bcrypt.hash(password, 10);
   console.log("hasPassword:", hasPassword);
 
-  console.log("registerAuth:", req.body);
   // db
+  const newUser = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hasPassword,
+    },
+  });
+  console.log("newUser:", newUser);
+  res.status(201).json({ message: "user created successfully" });
 };
 
 export const loginAuth = (req, res) => {
